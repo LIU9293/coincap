306c8fdd8d1c5808f346b51a4870ff1ecebf4548
liukai <lk@cobo.com>
2018-01-15 19:34:54 +0800

    update

--- src/db/index.js
+++ src/db/index.js
@@ -6,6 +6,7 @@ const connection = mysql.createConnection({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   multipleStatements: true,
+  inscure: process.env.DB_INSCURE,
 });
 
 module.exports = connection;
